451b5aacf718acc4280c1390ee3c89a1bc8a2e72
herrken <herrken@HerrKen.home>
2024-10-30 04:47:01 +0800

    First Commit

---
+++ nmapz.sh
@@ -0,0 +1,14 @@
+
+
+python3 /home/herrken/scripts/gen_nmap.py;
+
+while read item; do
+    # Extract IP and ports separately
+    ip=$(echo "$item" | awk '{print $1}')
+    ports=$(echo "$item" | awk -F '-p ' '{print $2}' | sed 's/,$//')
+
+    # Run nmap using the cleaned-up IP and port range
+    nmap -p "$ports" "$ip"
+done < nmap.txt
+
+rm mscan.txt nmap.txt
