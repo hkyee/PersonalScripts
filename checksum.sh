6dc0015070db6de8c1952819ce5c72bae7f3ddba
hkyee <yhken126@gmail.com>
2025-05-17 03:07:58 +0800

    17 May 2025

--- checksum.sh
+++ checksum.sh
@@ -15,7 +15,7 @@ read response
 if [[ "$response" =~ ^[Yy]$ ]]; then
     # If 'y' or 'Y', create MD5 checksums for all files in the current directory
     echo "Generating MD5 checksums for all files..."
-    find . -type f -exec md5sum {} + > md5sums.txt
+    find -L . -type f -exec md5sum {} + > md5sums.txt
 else
     # If anything other than 'y', quit the script
     echo "Quitting..."
